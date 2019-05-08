229b56208a83e745300a2cc2c4daf69d2df50274
Dmytro Yehorov <dm.yehorov@gmail.com>
2019-05-08 23:06:35 +0300

    Add homework 1 and 2 and 3

--- base_types_exceptions_loops/homework.py
+++ base_types_exceptions_loops/homework.py
@@ -15,7 +15,7 @@ def is_two_object_has_same_value(first: Any, second: Any) -> bool:
     If @first and @second has same value should return True
     In another case should return False
     """
-    pass
+    return first == second
 
 
 def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
@@ -23,7 +23,7 @@ def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    return type(first) == type(second)
 
 
 def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
@@ -31,7 +31,7 @@ def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    return first is second
 
 
 def multiple_ints(first_value: int, second_value: int) -> int:
@@ -48,20 +48,22 @@ def multiple_ints(first_value: int, second_value: int) -> int:
     Returns:
         Product of elements
     """
-    pass
+    if type(first_value) != int:
+        raise ValueError
+    return first_value * second_value
 
 
 def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
     """
     If possible to convert arguments to int value - convert and multiply them.
-    If it is impossible raise ValueError
+    If it is impossible raise OurAwesomeException
 
     Args:
         first_value: number for multiply
         second_value: number for multiply
 
     Raises:
-        ValueError
+        OurAwesomeException
 
     Returns: multiple of two numbers.
 
@@ -78,7 +80,10 @@ def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
             print("Not valid input data")
         >>> "Not valid input data"
     """
-    pass
+    try:
+        return int(first_value) * int(second_value)
+    except ValueError:
+        print("Not valid input data")
 
 
 def is_word_in_text(word: str, text: str) -> bool:
@@ -97,14 +102,14 @@ def is_word_in_text(word: str, text: str) -> bool:
         >>> False
 
     """
-    pass
+    return word in text
 
 
 def some_loop_exercise() -> list:
     """
     Use loop to create list that contain int values from 0 to 12 except 6 and 7
     """
-    pass
+    return [x for x in range(0, 13) if not x in (6, 7)]
 
 
 def remove_from_list_all_negative_numbers(data: List[int]) -> list:
@@ -116,7 +121,7 @@ def remove_from_list_all_negative_numbers(data: List[int]) -> list:
         remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
         >>> [1, 5, 8]
     """
-    pass
+    return [x for x in data if x >= 0]
 
 
 def alphabet() -> dict:
@@ -127,7 +132,7 @@ def alphabet() -> dict:
         alphabet()
         >>> {"a": 1, "b": 2 ...}
     """
-    pass
+    return {value - 96: chr(value) for value in range(ord('a'), ord('z') + 1)}
 
 
 def simple_sort(data: List[int]) -> List[list]:
@@ -139,4 +144,9 @@ def simple_sort(data: List[int]) -> List[list]:
     Returns:
 
     """
-    pass
+    out_list = []
+    for i in range(0, len(data)):
+        out_list.append(min(data))
+        data.remove(min(data))
+    return out_list
+
--- build_in_methods_iterators/homework.py
+++ build_in_methods_iterators/homework.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Union, Generator
-
+import random
+import string
 # We will work with such dicts
 ST = Dict[str, Union[str, int]]
 # And we will put this dicts in list
@@ -9,24 +10,23 @@ DT = List[ST]
 def task_1_fix_names_start_letter(data: DT) -> DT:
     """
     Make all `names` field in list of students to start from upper letter
-
     Examples:
         fix_names_start_letters([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}])
         >>> [{'name': 'Alex', 'age': 26}, {'name': 'Denys', 'age': 89}]
     """
-    pass
+    for member in data:
+        member.update((key, value.capitalize()) for key, value in member.items() if key == 'name')
+    return data
 
 
 def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
     """given_data
     Remove from dictionaries given key value
-
     Examples:
        remove_dict_field([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 'age')
         >>> [{'name': 'Alex'}, {'name': 'denys'}]
     """
-    pass
-
+    return [{key: value for key, value in member.items() if key not in redundant_keys} for member in data]
 
 def task_3_find_item_via_value(data: DT, value) -> DT:
     """
@@ -35,44 +35,44 @@ def task_3_find_item_via_value(data: DT, value) -> DT:
         find_item_via_value([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 26)
         >>> [{'name': 'Alex', 'age': 26}]
     """
-    pass
+    return [member for member in data if value in member.values()]
 
 
 def task_4_min_value_integers(data: List[int]) -> int:
     """
     Find and return minimum value from list
     """
-    pass
+    return min(data, default=None)
 
 
 def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
     """
     Find the longest string
     """
-    pass
+    return min([str(word).lower() for word in data], default=None)
 
 
 def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
     """
     Find minimum value by given key
     Returns:
-
     """
-    pass
+    new_list_of_dict = [members.get(key) for members in data if members.get(key) != None]
+    return data[new_list_of_dict.index(min(new_list_of_dict))]
 
 
 def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
     """
     Find max value from list of lists
     """
-    pass
+    return max([max(member) for member in data if member])
 
 
 def task_8_sum_of_ints(data: List[int]) -> int:
     """
     Find sum of all items in given list
     """
-    pass
+    return sum(data)
 
 
 def task_9_sum_characters_positions(text: str) -> int:
@@ -80,15 +80,13 @@ def task_9_sum_characters_positions(text: str) -> int:
     Please read first about ascii table.
     https://python-reference.readthedocs.io/en/latest/docs/str/ASCII.html
     You need to calculate sum of decimal value of each symbol in text
-
     Examples:
         task_9_sum_characters_positions("A")
         >>> 65
         task_9_sum_characters_positions("hello")
         >>> 532
-
     """
-    pass
+    return sum([ord(value) for value in text])
 
 
 def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
@@ -102,12 +100,32 @@ def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
         next(a)
         >>> 3
     """
-    pass
+
+    def generat_sim_num():
+        lst = [i for i in range(2, 200)]
+        for i in range(len(lst)):
+            if lst[i] != 0:
+                for j in range(i + 1, len(lst)):
+                    if lst[j] != 0:
+                        if (lst[j] % lst[i] == 0):
+                            lst[lst.index(lst[j])] = 0
+        return [mem for mem in lst if mem != 0]
+
+
+    def check_sim_num():
+        n = 2
+        while n < 200:
+            if n in generat_sim_num():
+                yield n
+            n += 1
+
+
+    return check_sim_num()
 
 
 def task_11_create_list_of_random_characters() -> List[str]:
     """
     Create list of 20 elements where each element is random letter from latin alphabet
-
     """
-    pass
+    return [random.choice(string.ascii_lowercase) for _ in range(20)]
+
