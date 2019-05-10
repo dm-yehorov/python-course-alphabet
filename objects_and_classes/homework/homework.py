5373d1ec37efc43bec48c6b67746f5fbb83c9a6a
Dmytro Yehorov <dm.yehorov@gmail.com>
2019-05-10 08:19:04 +0300

    Procces to do HM4

--- objects_and_classes/homework/homework.py
+++ objects_and_classes/homework/homework.py
@@ -1,3 +1,10 @@
+from constants import *
+from typing import List
+
+import random
+import uuid
+
+
 """
 Вам небхідно написати 3 класи. Колекціонери Гаражі та Автомобілі.
 Звязкок наступний один колекціонер може мати багато гаражів.
@@ -49,12 +56,112 @@
 
 
 class Cesar:
-    pass
 
+    def __init__(self, name, register_id, garages= []):
+        self.name = name
+        self.register_id = register_id
+        self.garages = garages
+
+
+    def hit_hat(self):
+        pass
+
+
+    def garages_count(self):
+        return len(self.garages)
+
+
+    def сars_count(self):
+        #for i in self.garages:
+        pass
+
+
+    def add_car(self):
+        pass
+
+
+    def display_all (self):
+        print(self.name)
+        print(self.register_id)
+        print(self.garages)
+        print("#####################")
 
 class Car:
-    pass
+
+    def __init__(self):
+        self.price = random.randrange(800,2000)
+        self.type = random.choice(CARS_TYPES)
+        self.producer = random.choice(CARS_PRODUCER)
+        self.number = uuid.uuid4().hex
+        self.milleage = random.randrange(1000)
+
+
+    def __str__(self):
+        return f"Price: {self.price}, Type: {self.type}, Producer: {self.producer}, Number: {self.number}, Milleage: {self.milleage}"
 
 
 class Garage:
-    pass
+
+    def __init__(self, town, owner):
+        self.town = town
+        self.cars = [Car() for _ in range(random.randrange(5))]
+        self.places = len(self.cars) + 5
+        self.owner = owner
+
+
+    def hit_hat(self):
+        pass
+
+    def __str__(self):
+        return f"Cars in garages: {len(self.cars)}, Free places is: {self.places - len(self.cars)}, Owner is: {self.owner} "
+
+
+if __name__ == '__main__':
+
+    "Init First and second Cesar"
+
+    Tom = Cesar(
+        name='Tom',
+        register_id= uuid.uuid4().hex,
+        garages= [random.choice(TOWNS) for x in range(random.randrange(1,3))]
+                )
+
+    Jack = Cesar(
+        name='Jack',
+        register_id= uuid.uuid4().hex,
+        garages= [random.choice(TOWNS) for x in range(random.randrange(1,3))]
+        )
+
+    "Init Garages and Cars for each Cesar"
+    "---Tom's garages"
+
+    tom_garages_lst = []
+    for i in range(len(Tom.garages)):
+        toms_garage = Garage(
+            town= Tom.garages[i],
+            owner= Tom.register_id
+       )
+        tom_garages_lst.append(toms_garage)
+
+
+    "---Jack's garages"
+
+    jack_garages_lst = []
+    for i in range(len(Jack.garages)):
+        jacks_garage = Garage(
+            town= Jack.garages[i],
+            owner= Jack.register_id
+       )
+        jack_garages_lst.append(jacks_garage)
+
+
+    print(Tom.display_all())
+    print(Tom.garages_count())
+    print(Tom.g())
+    """
+    f
+    
+    
+    """
+
+
