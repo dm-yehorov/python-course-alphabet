3f7a1996d7ba592742a05019c9820c54adb5f550
dm-yehorov <49338141+dm-yehorov@users.noreply.github.com>
2019-05-10 17:46:15 +0300

    Done Homework.py

--- objects_and_classes/homework/homework.py
+++ objects_and_classes/homework/homework.py
@@ -57,17 +57,18 @@ import uuid
 
 class Cesar:
 
-    def __init__(self, name, register_id, garages= []):
+    def __init__(self, name):
         self.name = name
-        self.register_id = register_id
-        self.garages = garages
+        self.register_id = uuid.uuid4().hex
+        self.garages = [Garage(owner= self.register_id) for _ in range(random.randrange(1,3))]
 
 
     def hit_hat(self):
         total_price_cars = 0
         for x in self.garages:
-            tom_garages_lst += len(x)
-        return tom_garages_lst
+            for y in range(len(x.cars)):
+                total_price_cars += x.cars[y].price
+        return total_price_cars
 
 
     def garages_count(self):
@@ -77,19 +78,17 @@ class Cesar:
     def сars_count(self):
         count_car = 0
         for x in self.garages:
-            count_cat += len(x)
+            count_car += len(x.cars)
         return count_car
 
 
     def add_car(self):
-        pass
+       free_places_lst = [i.free_places() for i in self.garages ]
+       if max(free_places_lst) > 0:
+            self.garages[free_places_lst.index(max(free_places_lst))].cars.append(Car())
+       else:
+            return ('Not free places in any garages')
 
-
-    def display_all (self):
-        print(self.name)
-        print(self.register_id)
-        print(self.garages)
-        print("#####################")
 
 class Car:
 
@@ -101,75 +100,119 @@ class Car:
         self.milleage = random.randrange(1000)
 
 
+    def chg_num(self):
+        self.number = uuid.uuid4().hex
+
+
     def __str__(self):
         return f"Price: {self.price}, Type: {self.type}, Producer: {self.producer}, Number: {self.number}, Milleage: {self.milleage}"
 
 
 class Garage:
 
-    def __init__(self, town, owner):
-        self.town = town
+    def __init__(self, owner):
+        self.town = random.choice(TOWNS)
         self.cars = [Car() for _ in range(random.randrange(5))]
-        self.places = len(self.cars) + 5
+        self.places = len(self.cars) + random.randrange(5)
         self.owner = owner
 
 
-    def hit_hat(self):
-        pass
+    def add(self):
+        if self.places - len(self.cars) > 0:
+            self.cars.append(Car())
+        else:
+            print('I dont have free places')
 
-    def __str__(self):
-        return f"Cars in garages: {len(self.cars)}, Free places is: {self.places - len(self.cars)}, Owner is: {self.owner} "
+
+    def remove(self):
+        if len(self.cars) > 0:
+            self.cars.pop(random.randrange(len(self.cars)))
+
+
+    def hit_hat(self):
+        total_price_cars = 0
+        for i in range(len(self.cars)):
+            total_price_cars += self.cars[i].price
+        return total_price_cars
+
+
+    def free_places(self):
+        return self.places - len(self.cars)
 
 
 if __name__ == '__main__':
 
-    "Init First and second Cesar"
-
-    Tom = Cesar(
-        name='Tom',
-        register_id= uuid.uuid4().hex,
-        garages= [random.choice(TOWNS) for x in range(random.randrange(1,3))]
-                )
-
-    Jack = Cesar(
-        name='Jack',
-        register_id= uuid.uuid4().hex,
-        garages= [random.choice(TOWNS) for x in range(random.randrange(1,3))]
-        )
-
-    "Init Garages and Cars for each Cesar"
-    "---Tom's garages"
-
-    tom_garages_lst = []
-    for i in range(len(Tom.garages)):
-        toms_garage = Garage(
-            town= Tom.garages[i],
-            owner= Tom.register_id
-       )
-        tom_garages_lst.append(toms_garage)
-
-
-    "---Jack's garages"
-
-    jack_garages_lst = []
-    for i in range(len(Jack.garages)):
-        jacks_garage = Garage(
-            town= Jack.garages[i],
-            owner= Jack.register_id
-       )
-        jack_garages_lst.append(jacks_garage)
-
-
-    print(Tom.display_all())
-    #print(Tom.garages_count())
-    print('Kol garges is ' + str(Tom.garages_count()))
-    print('Kol cars is ' + str(tom_garages_lst[0].cars[0].price))
     """
-    for garage in :
-        for car in garage:
-        
-    
-    
-    """
+    Init First and second Cesar. For each Cesar generate Garages and Cars
 
+    """
+    Tom = Cesar(name='Tom')
+    Jack = Cesar(name='Jack')
+
+    " Lets show number of garages and cars "
+
+    print('Count Garages for Tom ' + str(Tom.garages_count()))
+    print('Count Garages for Jack ' + str(Jack.garages_count()))
+    print('Count Cars for Tom ' + str(Tom.сars_count()))
+    print('Count Cars for Jack ' + str(Jack.сars_count()))
+
+    "Hit_hat fo each Cesar"
+
+    print('Sum money all cars for Tom ' + str(Tom.hit_hat()))
+    print('Sum money all cars for Jack ' + str(Jack.hit_hat()))
+
+    "Hit_hat fo each Garase for Cesar"
+    "Tom's garages"
+    for i in range(Tom.garages_count()):
+        print('Sum money all cars in each Garages for Tom ' + str(Tom.garages[i].hit_hat()))
+
+    "Jack's garages"
+    for i in range(Jack.garages_count()):
+        print('Sum money all cars in each Garages for Jack ' + str(Jack.garages[i].hit_hat()))
+
+    "Logs car in each Garages for Tom"
+    print("For Toms")
+    for i in Tom.garages:
+        for j in i.cars:
+            print(j)
+    "Logs car in each Garages for Jack"
+    print("For Jack")
+    for i in Jack.garages:
+        for j in i.cars:
+            print(j)
+
+    print("Add Cars to Tom.Garages")
+    Tom.add_car()
+    for i in Tom.garages:
+        for j in i.cars:
+            print(j)
+
+    print("Add Cars to Jack.Garages")
+    Jack.add_car()
+    for i in Jack.garages:
+        for j in i.cars:
+            print(j)
+
+    print("Remove Cars to Tom.Garages")
+    for i in Tom.garages:
+        if len(i.cars) > 0:
+            i.cars.pop(random.randrange(len(i.cars)))
+            break
+    for i in Tom.garages:
+        for j in i.cars:
+            print(j)
+
+    print("Remove Cars to Jack.Garages")
+    for i in Jack.garages:
+        if len(i.cars) > 0:
+            i.cars.pop(random.randrange(len(i.cars)))
+            break
+    for i in Jack.garages:
+        for j in i.cars:
+            print(j)
+
+    print("Change number for random car in random for Tom")
+    print(Tom.garages[0].cars[0])
+    Tom.garages[0].cars[0].chg_num()
+    print(Tom.garages[0].cars[0])
 
